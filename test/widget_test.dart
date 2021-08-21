2e114b0ec4486da5e6c0b851124fa54473e63992
HillRod <abam_espana@hotmail.com>
2021-08-21 17:04:27 -0500

    App Name updated to Future Words

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:english_rememberer/main.dart';
+import 'package:future_words/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
